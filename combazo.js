8077384661d15a282c1d6020475045795c1ac144
Hernan Bermudez <hbermudez.info@gmail.com>
2021-01-31 00:29:59 -0500

    Calculadora_con_botones

--- combazo.js
+++ combazo.js
@@ -1,5 +1,59 @@
 
-function calculadora(){
+function cero(){
+    let num0 = 0;
+    const cerotxt = toString.num0;
+}
+function uno(){
+    let num1 = 1;
+    const unotxt = toString.num1;
+}
+function dos(){
+    let num2 = 2;
+    const dostxt = toString.num2;
+}
+function tres(){
+    let num3 = 3;
+    const trestxt = toString.num3;
+}
+function cuatro(){
+    let num4 = 4;
+    const cuatrotxt = toString.num4;
+}
+function cinco(){
+    let num5 = 5;
+    const cincotxt = toString.num5;
+}
+function seis(){
+    let num6 = 6;
+    const seistxt = toString.num6;
+}
+function siete(){
+    let num7 = 7;
+    const sietetxt = toString.num7;
+}
+function ocho(){
+    let num8 = 8;
+    const ochotxt = toString.num8;
+}
+function nueve(){
+    let num9 = 9;
+    const nuevetxt = toString.num9;
+}
+
+function pantalla(){
+    texto = [];
+    var parada = new Boolean (false);
+    while (parada != true);
+    for(let i=0;i<10;i++){
+
+    }
+    String operando = toString.cero;
+
+}
+
+
+
+/*function calculadora(){
 
     let num1 = parseFloat(document.getElementById('number1').value);
     let num2 = parseFloat(document.getElementById('number2').value);
@@ -18,5 +72,7 @@ function calculadora(){
     if(operacion === 'divi'){
         document.getElementById('result').value = (num1 / num2); 
     }
-}
+}*/
+
+
 
